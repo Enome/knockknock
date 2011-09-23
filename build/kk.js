d2c567a0ee5c3011e3e63c8e1dbb88b984a29fcc
Geert Pasteels <geert.pasteels@gmail.com>
2011-09-23 12:33:21 +0200

    Great forgot to build

--- build/kk.js
+++ build/kk.js
@@ -8,8 +8,8 @@ var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, par
   return child;
 }, __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; }, __slice = Array.prototype.slice;
 Required = (function() {
-  function Required() {
-    this.message = 'This field is required.';
+  function Required(message) {
+    this.message = message || 'This field is required.';
   }
   Required.prototype.validate = function(observable) {
     var value;
@@ -19,9 +19,9 @@ Required = (function() {
   return Required;
 })();
 Max = (function() {
-  function Max(length) {
+  function Max(length, message) {
     this.length = length;
-    this.message = "Please enter no more than " + this.length + " character(s).";
+    this.message = message || ("Please enter no more than " + this.length + " character(s).");
   }
   Max.prototype.validate = function(observable) {
     var value;
@@ -31,9 +31,9 @@ Max = (function() {
   return Max;
 })();
 Regex = (function() {
-  function Regex(rx) {
+  function Regex(rx, message) {
     this.rx = rx;
-    this.message = 'Your entry didn\'t match the pattern.';
+    this.message = message || 'Your entry didn\'t match the pattern.';
   }
   Regex.prototype.validate = function(observable) {
     var value;
@@ -44,24 +44,24 @@ Regex = (function() {
 })();
 Email = (function() {
   __extends(Email, Regex);
-  function Email() {
+  function Email(message) {
     Email.__super__.constructor.call(this, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-    this.message = 'Your entry didn\'t match a valid emailaddress.';
+    this.message = message || 'Your entry didn\'t match a valid emailaddress.';
   }
   return Email;
 })();
 Number = (function() {
   __extends(Number, Regex);
-  function Number() {
+  function Number(message) {
     Number.__super__.constructor.call(this, /^-?(?:\d+|\d{1,3}(?:,\d{3})+)(?:\.\d+)?$/);
-    this.message = 'Your entry didn\'t match a valid number ( 1 1.0 1,000.00 -1 -1.0 ... )';
+    this.message = message || 'Your entry didn\'t match a valid number ( 1 1.0 1,000.00 -1 -1.0 ... )';
   }
   return Number;
 })();
 Equals = (function() {
-  function Equals(equalee) {
+  function Equals(equalee, message) {
     this.equalee = equalee;
-    this.message = 'Your entry didn\'t match the other value.';
+    this.message = message || 'Your entry didn\'t match the other value.';
   }
   Equals.prototype.validate = function(observable) {
     if (typeof this.equalee === 'function') {
